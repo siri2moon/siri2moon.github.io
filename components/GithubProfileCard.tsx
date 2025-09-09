1acb22f1f0c345aa7974eba19ff2e0aac65208b3
siri2moon <siri2moon@gmail.com>
2025-09-09 21:02:26 +0700

    feat: deploy cv

--- components/GithubProfileCard.tsx
+++ components/GithubProfileCard.tsx
@@ -20,14 +20,9 @@ const GithubProfileCard = ({ avatar_url, bio, location }: GithubUserType) => {
             <Col lg="8" className="order-lg-1">
               <h2 className="text-white">Reach Out to me!</h2>
               <p className="lead text-white mt-3">
-                DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR
-                ALL
+                DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL
               </p>
               <p className="text-white mt-3">{bio}</p>
-              <div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
-                <i className="ni ni-pin-3 text-info mr-2" />
-                {location}
-              </div>
               <SocialLinks />
             </Col>
           </Row>
